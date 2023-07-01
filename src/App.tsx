edb57e266a32763a68cb73796e78ab0b8bde3db8
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-07-01 20:54:01 -0300

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import { Header } from './Components/header/header.styled'
 import { Logo } from './Components/header/header_logo.styled'
 import React from 'react'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: '/character/:id',
+    element: <div>Character!</div>,
+  },
+  {
+    path: '/*',
+    element: <div>404</div>,
+  },
+])
+
 const App: React.FC = () => {
   return (
-    <Header>
-      <Logo src="/assets/images/logo.svg" alt="Logo" />
-    </Header>
+    <div>
+      <Header>
+        <Logo src="/assets/images/logo.svg" alt="Logo" />
+      </Header>
+      <main>
+        <RouterProvider router={router} />
+      </main>
+    </div>
   )
 }
 
